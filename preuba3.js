3d4069ba7feda02d017485db03c503d371c97f34
Facu <facuruizbrun1998@gmail.com>
2025-08-13 16:00:37 -0300

    funciones1

---
+++ preuba3.js
@@ -0,0 +1,11 @@
+function calcularAreaRectangulo(base,altura){
+    let area = base * altura;
+    
+};
+
+
+
+let base = parseInt(prompt('ingrese el tamaño de la base: '));
+let altura = parseInt(prompt('ingrese la altura: '));
+console.log(`el area es ${area}`);
+calcularAreaRectangulo(base,altura);
