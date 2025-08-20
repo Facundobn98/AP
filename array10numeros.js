b8a6694c866ebdddf5e79ce8693826acd98d6591
Facu <facuruizbrun1998@gmail.com>
2025-08-20 15:25:08 -0300

    practicando arrays y bucles

---
+++ array10numeros.js
@@ -0,0 +1,29 @@
+
+
+//Imprime en consola:
+
+//Todos los números
+
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(numeros)
+
+//La suma de los número
+
+let datosN =  [1,2,3,4]
+let total;
+
+for (let i = 0;i < datosN.length; i++){
+    total = datosN[i];
+}
+console.log(total);
+
+//El promedio
+
+let datosNN = [4,3,2,0];
+let total2;
+
+for (let i = 0; i < datosNN.length; i++){
+    total2 = datosNN[i] / datosNN.length;
+}
+console.log(total2)
